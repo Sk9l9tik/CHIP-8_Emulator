c16bfa34bad01fb20d1b3bec41722fc4b5db496a
crw448 <popadkin_mm@edu.surgu.ru>
2026-07-03 16:32:47 +0500

    feat: added SFML main cycle

--- include/ui/widgets/Display.h
+++ include/ui/widgets/Display.h
@@ -8,7 +8,7 @@ public:
     static constexpr unsigned char DISPLAY_WIDTH = 64;
     static constexpr unsigned char DISPLAY_HEIGHT = 32;
 
-    Display() : pixel_data(DISPLAY_WIDTH * DISPLAY_HEIGHT) {
+    explicit Display(const FrameBuffer &data) : pixel_data(data) {
         d_texture.clear();
         d_texture.display();
 
@@ -23,7 +23,7 @@ public:
         // ну неважно в принципе
     }
 
-    void update() override {
+    void update() override { // че
         update_texture();
     }
 
@@ -33,39 +33,37 @@ public:
 
     //
 
-    void set_pixel(uint8_t x, uint8_t y, bool state){
-        pixel_data[x+y*DISPLAY_WIDTH] = state;
-    }
+//    void set_pixel(uint8_t x, uint8_t y, bool state){
+//        pixel_data->get_frame_buffer()[x+y*DISPLAY_WIDTH] = state;
+//    }
 
     bool get_pixel(uint8_t x, uint8_t y) const {
-        return pixel_data[x+y*DISPLAY_WIDTH];
+        return pixel_data.get_frame_buffer()[x+y*DISPLAY_WIDTH];
     }
 
-    void fill(bool state){
-        std::fill(pixel_data.begin(), pixel_data.end(), state);
-    }
-
-    void clear(){
-        fill(false);
-    }
+//    void fill(bool state){
+//        std::fill(pixel_data.begin(), pixel_data.end(), state);
+//    }
+//
+//    void clear(){
+//        fill(false);
+//    }
 
     void set_pixel_size(float p_size){
         pixel_size = p_size;
     }
 
+
+private:
     void update_texture(){
         d_texture.clear(sf::Color::Black);
 
         sf::RectangleShape pixel(sf::Vector2f(1.f, 1.f));
-        //TEST
-        for(auto p : pixel_data){
-            p = rand()%2 == 0;
-        }
-        //
+
         for (unsigned int y = 0; y < DISPLAY_HEIGHT; y++) {
             for (unsigned int x = 0; x < DISPLAY_WIDTH; x++) {
                 pixel.setPosition({static_cast<float>(x), static_cast<float>(y)});
-                pixel.setFillColor(pixel_data[x + y * DISPLAY_WIDTH] ? sf::Color::Black : sf::Color::White);
+                pixel.setFillColor(pixel_data.get_frame_buffer()[x + y * DISPLAY_WIDTH] ? sf::Color::Black : sf::Color::White);
                 d_texture.draw(pixel);
             }
         }
@@ -77,10 +75,9 @@ public:
         d_sprite.setScale({pixel_size, pixel_size}); // )
     }
 
-private:
     sf::RenderTexture d_texture{{DISPLAY_WIDTH, DISPLAY_HEIGHT}};
     sf::Sprite d_sprite{d_texture.getTexture()};
-    std::vector<bool> pixel_data{};
+    const FrameBuffer &pixel_data;
     float pixel_size = 10.f;
 };
 #endif //CHIP_8_EMULATOR_DISPLAY_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 #include "core/CHIP_8.h"
 
+#include "ui/GUI.h"
+#include "ui/widgets/Display.h"
+
 #define CHIP8_DEBUG1
 
 #ifdef CHIP8_DEBUG
@@ -12,8 +15,16 @@
 
 int main(){
     CHIP_8 emulator {};
-    emulator.load_ROM("IBM_Logo.ch8"); //укажите здесь пути до этого прикола свои
+    emulator.load_ROM("3-corax+.ch8"); //укажите здесь пути до этого прикола свои
 
+    sf::RenderWindow window(sf::VideoMode({640, 320}), "CHIP-8 Emulator");
+    window.setFramerateLimit(60);
+    GUI gui{&window};
+
+    Display display{emulator.get_frame_buffer()};
+    display.set_size({640, 320});
+
+    gui.add(&display);
 #ifdef CHIP8_DEBUG
     Debugger d(emulator);
     
@@ -32,5 +43,25 @@ int main(){
 //        printf("%04X: %02X\n", 0x200 + i, emulator.get_memory().raw()[0x200+i]);
 //    emulator.tick();
 //    emulator.tick();
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            gui.handle_event(event);
+        }
+
+        emulator.tick();
+
+        window.clear(sf::Color::Black);
+
+        gui.update();
+        gui.render();
+
+        window.display();
+    }
+
     return 0;
 }
