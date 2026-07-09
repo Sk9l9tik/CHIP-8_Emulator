69f12df0ee3d1cc304db5874b46aa21d1a516afa
Sk9l9tik <adbelosheev@gmail.com>
2026-07-09 14:30:29 +0300

    feat: move things to utils.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
 #include <cmath>
 
-#include <SFML/Audio.hpp>
+#include <iostream>
+
+#include "utils.hpp"
 
 #include "core/CHIP_8.h"
 #include "core/Memory.h"
@@ -20,149 +22,6 @@
 
 // Пишет в консоль вообще по каждому SFML событию, можете в отдельный файл как то выделить типа
 // tools или misc или helpers или utils или че там пишут я забыл уже но мне лично лень просто
-void SAERMO_logger(const std::optional<sf::Event>& event)
-{
-    // Проверка, что событие существует
-    if (!event.has_value())
-    {
-        std::cout << "Event: nullopt" << std::endl;
-        return;
-    }
-
-    // Закрытие окна
-    if (event->is<sf::Event::Closed>())
-    {
-        std::cout << "Event: Closed" << std::endl;
-    }
-        // Изменение размера
-    else if (const auto* resized = event->getIf<sf::Event::Resized>())
-    {
-        std::cout << "Event: Resized (width=" << resized->size.x
-                  << ", height=" << resized->size.y << ")" << std::endl;
-    }
-        // Потеря фокуса
-    else if (event->is<sf::Event::FocusLost>())  // было event.is -> event->is
-    {
-        std::cout << "Event: FocusLost" << std::endl;
-    }
-        // Получение фокуса
-    else if (event->is<sf::Event::FocusGained>())  // было event.is -> event->is
-    {
-        std::cout << "Event: FocusGained" << std::endl;
-    }
-
-        // === Клавиатура ===
-    else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
-    {
-        std::cout << "Event: KeyPressed (code=" << static_cast<int>(keyPressed->code)
-                  << ", scancode=" << static_cast<int>(keyPressed->scancode)
-                  << ", alt=" << keyPressed->alt
-                  << ", control=" << keyPressed->control
-                  << ", shift=" << keyPressed->shift
-                  << ", system=" << keyPressed->system << ")" << std::endl;
-    }
-    else if (const auto* keyReleased = event->getIf<sf::Event::KeyReleased>())
-    {
-        std::cout << "Event: KeyReleased (code=" << static_cast<int>(keyReleased->code)
-                  << ", scancode=" << static_cast<int>(keyReleased->scancode)
-                  << ", alt=" << keyReleased->alt
-                  << ", control=" << keyReleased->control
-                  << ", shift=" << keyReleased->shift
-                  << ", system=" << keyReleased->system << ")" << std::endl;
-    }
-    else if (const auto* textEntered = event->getIf<sf::Event::TextEntered>())
-    {
-        std::cout << "Event: TextEntered (unicode=" << static_cast<int>(textEntered->unicode)
-                  << ", char='" << static_cast<char>(textEntered->unicode) << "')" << std::endl;
-    }
-
-        // === Мышь ===
-//    else if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
-//    {
-//        std::cout << "Event: MouseMoved (x=" << mouseMoved->position.x
-//                  << ", y=" << mouseMoved->position.y << ")" << std::endl;
-//    }
-//    else if (const auto* mouseMovedRaw = event->getIf<sf::Event::MouseMovedRaw>())
-//    {
-//        std::cout << "Event: MouseMovedRaw (dx=" << mouseMovedRaw->delta.x
-//                  << ", dy=" << mouseMovedRaw->delta.y << ")" << std::endl;
-//    }
-    else if (const auto* mousePressed = event->getIf<sf::Event::MouseButtonPressed>())
-    {
-        std::cout << "Event: MouseButtonPressed (button=" << static_cast<int>(mousePressed->button)
-                  << ", x=" << mousePressed->position.x
-                  << ", y=" << mousePressed->position.y << ")" << std::endl;
-    }
-    else if (const auto* mouseReleased = event->getIf<sf::Event::MouseButtonReleased>())
-    {
-        std::cout << "Event: MouseButtonReleased (button=" << static_cast<int>(mouseReleased->button)
-                  << ", x=" << mouseReleased->position.x
-                  << ", y=" << mouseReleased->position.y << ")" << std::endl;
-    }
-    else if (const auto* wheelScrolled = event->getIf<sf::Event::MouseWheelScrolled>())
-    {
-        std::cout << "Event: MouseWheelScrolled (wheel=" << static_cast<int>(wheelScrolled->wheel)
-                  << ", delta=" << wheelScrolled->delta
-                  << ", x=" << wheelScrolled->position.x
-                  << ", y=" << wheelScrolled->position.y << ")" << std::endl;
-    }
-    else if (event->is<sf::Event::MouseEntered>())  // было event.is -> event->is
-    {
-        std::cout << "Event: MouseEntered" << std::endl;
-    }
-    else if (event->is<sf::Event::MouseLeft>())  // было event.is -> event->is
-    {
-        std::cout << "Event: MouseLeft" << std::endl;
-    }
-
-        // === Джойстик ===
-    else if (event->is<sf::Event::JoystickConnected>())  // было event.is -> event->is
-    {
-        std::cout << "Event: JoystickConnected (id="
-                  << event->getIf<sf::Event::JoystickConnected>()->joystickId << ")" << std::endl;
-    }
-    else if (event->is<sf::Event::JoystickDisconnected>())  // было event.is -> event->is
-    {
-        std::cout << "Event: JoystickDisconnected (id="
-                  << event->getIf<sf::Event::JoystickDisconnected>()->joystickId << ")" << std::endl;
-    }
-    else if (const auto* joyMoved = event->getIf<sf::Event::JoystickMoved>())
-    {
-        std::cout << "Event: JoystickMoved (id=" << joyMoved->joystickId
-                  << ", axis=" << static_cast<int>(joyMoved->axis)
-                  << ", position=" << joyMoved->position << ")" << std::endl;
-    }
-    else if (const auto* joyPressed = event->getIf<sf::Event::JoystickButtonPressed>())
-    {
-        std::cout << "Event: JoystickButtonPressed (id=" << joyPressed->joystickId
-                  << ", button=" << joyPressed->button << ")" << std::endl;
-    }
-    else if (const auto* joyReleased = event->getIf<sf::Event::JoystickButtonReleased>())
-    {
-        std::cout << "Event: JoystickButtonReleased (id=" << joyReleased->joystickId
-                  << ", button=" << joyReleased->button << ")" << std::endl;
-    }
-
-        // === Сенсорный ввод ===
-    else if (const auto* touchBegan = event->getIf<sf::Event::TouchBegan>())
-    {
-        std::cout << "Event: TouchBegan (finger=" << touchBegan->finger
-                  << ", x=" << touchBegan->position.x
-                  << ", y=" << touchBegan->position.y << ")" << std::endl;
-    }
-    else if (const auto* touchMoved = event->getIf<sf::Event::TouchMoved>())
-    {
-        std::cout << "Event: TouchMoved (finger=" << touchMoved->finger
-                  << ", x=" << touchMoved->position.x
-                  << ", y=" << touchMoved->position.y << ")" << std::endl;
-    }
-    else if (const auto* touchEnded = event->getIf<sf::Event::TouchEnded>())
-    {
-        std::cout << "Event: TouchEnded (finger=" << touchEnded->finger
-                  << ", x=" << touchEnded->position.x
-                  << ", y=" << touchEnded->position.y << ")" << std::endl;
-    }
-}
 
 int main(int argc, char* argv[]){
 
@@ -381,7 +240,9 @@ int main(int argc, char* argv[]){
                 }
         }
 #endif
-            //SAERMO_logger(event);
+#ifdef DEBUG
+        utils::SAERMO_logger(event);
+#endif
 
             gui.handle_event(event, sf::Mouse::getPosition(window));
         }
